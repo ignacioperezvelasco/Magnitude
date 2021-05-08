581c36d3ed349b20b7e8c67f3a6ed489632cb576
Sergio E <Sergio.escuder5@gmail.com>
2021-05-08 13:50:21 +0200

    Labyrinth Logic DONE

---
+++ Assets/LabyrinthGestion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthGestion : MonoBehaviour
+{
+    public WallGestion[] walls;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (WallGestion wall in walls)
+            {
+                wall.ChangePositioon();
+            }
+        }
+    }
+}

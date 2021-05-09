00bbba5549e14e26aaa5f26e0c88edc07be703df
Sergio E <Sergio.escuder5@gmail.com>
2021-05-09 12:41:56 +0200

    Right Room DONE

---
+++ Assets/PlatformRighRoomManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRighRoomManager : MonoBehaviour
+{
+    [SerializeField] ImanBehavior imanObject;
+    public RightRoomPlatform[] platforms;
+    iman currentPole;
+    bool isActivated = false;
+
+    private void Start()
+    {
+        currentPole = imanObject.myPole;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActivated)
+        {
+            if (currentPole != imanObject.myPole)
+            {
+                currentPole = imanObject.myPole;
+
+                ChangePlatformPosition();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ActivateRoom();
+        }
+    }
+
+    void ChangePlatformPosition()
+    {
+        foreach (RightRoomPlatform platform in platforms)
+        {
+            //Llamamos la funcion de cambiar de posicion
+            platform.ChangePosition();
+        }
+    }
+
+    void ActivatePlatforms()
+    {
+        foreach (RightRoomPlatform platform in platforms)
+        {
+            //Llamamos la funcion de cambiar de posicion
+            if (platform.type == RightRoomPlatform.PlatformType.NEGATIVE)
+            {
+                platform.ChangePosition();
+            }
+        }
+    }
+
+    public void ActivateRoom()
+    {
+        isActivated = true;
+
+        ActivatePlatforms();
+    }
+}
---
+++ Assets/RightRoomPlatform.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RightRoomPlatform : MonoBehaviour
+{
+    public enum PlatformType
+    {
+        POSITIVE,
+        NEGATIVE
+    };
+
+    public enum AxisMovement
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    };
+
+    Vector3 initialPosition;
+    Vector3 closedPosition;
+
+    public PlatformType type;
+    [SerializeField] AxisMovement axis;
+    [SerializeField] float speed;
+    float amountToMove = 6;
+    bool active = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = this.transform.position;
+                
+        switch (axis)
+        {
+            case AxisMovement.LEFT:
+                {
+                    closedPosition = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z - amountToMove);
+                    break;
+                }
+
+            case AxisMovement.RIGHT:
+                {
+                    closedPosition = new Vector3( initialPosition.x , initialPosition.y , initialPosition.z + amountToMove );
+                    break;
+                }
+            case AxisMovement.UP:
+                {
+                    closedPosition = new Vector3( initialPosition.x - amountToMove , initialPosition.y , initialPosition.z );
+                    break;
+                }
+            case AxisMovement.DOWN:
+                {
+                    closedPosition = new Vector3 ( initialPosition.x + amountToMove , initialPosition.y , initialPosition.z );
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
+    public void ChangePosition()
+    {
+        if (active)
+        {
+            active = false;
+
+            this.transform.DOMove(closedPosition, speed);
+        }
+        else
+        {
+            active = true;
+            this.transform.DOMove(initialPosition, speed);           
+        }
+    }
+}

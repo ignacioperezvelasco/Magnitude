ec8ad8f37d016e8411955fd070e42513dafdf867
Sergio E <Sergio.escuder5@gmail.com>
2021-05-12 20:29:10 +0200

    Camera Transition Level 2 DONE

--- Assets/RoomCentreManager.cs
+++ Assets/RoomCentreManager.cs
@@ -14,6 +14,39 @@ public class RoomCentreManager : MonoBehaviour
     [SerializeField] ElevatorLevel2 elevator;
     [SerializeField] DoorManager door;
 
+    [Header("TURRET")]
+    [SerializeField] TurretEnemy turret;
+    [SerializeField] GameObject transitionDoor;
+    bool transitionActivated = false;
+
+    [Header("TO DEACTIVATE")]    
+    [SerializeField] GameObject transitionRoom1_D;
+    [SerializeField] GameObject transitionRoom2_D;
+    [SerializeField] GameObject transitionRoom3_D;
+
+    [Header("TO ACTIVATE")]
+    [SerializeField] GameObject transitionRoom1_A;
+    [SerializeField] GameObject transitionRoom2_A;
+    [SerializeField] GameObject transitionRoom3_A;
+
+    private void Update()
+    {
+        if (!turret.headActivate && !transitionActivated)
+        {
+            transitionActivated = true;
+            transitionDoor.SetActive(true);
+
+            //Cambiamos las transiciones de cámara
+            transitionRoom1_D.SetActive(false);
+            transitionRoom2_D.SetActive(false);
+            transitionRoom3_D.SetActive(false);
+
+            transitionRoom1_A.SetActive(true);
+            transitionRoom2_A.SetActive(true);
+            transitionRoom3_A.SetActive(true);
+        }
+    }
+
     public void ActivateSwitch(int _numswitch)
     {
         switch (_numswitch)

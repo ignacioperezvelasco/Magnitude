cde7022ba45d7fb4a040702ebf7a8c968a765192
Sergio E <Sergio.escuder5@gmail.com>
2021-05-16 20:28:58 +0200

    Finishing Touches & Adding New Assets

---
+++ Assets/LoadLastScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadLastScreen : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+    [SerializeField] Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Invoke("LoadScene", 3);
+        }
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+           
+    }
+}

1b9bc50b794fbf49063fbcab202c7eeb30984c8f
Martí Gómez Prunera <magopru1995@gmail.com>
2021-06-18 10:54:47 +0200

    Cutscene

---
+++ Assets/_PROJECT/SCRIPTS/CutScenePlay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CutScenePlay : MonoBehaviour
+{
+    public VideoPlayer cutscene;
+
+    void Start()
+    {
+        cutscene.loopPointReached += CheckOver;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
